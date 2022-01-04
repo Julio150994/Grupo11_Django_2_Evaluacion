004669c0aa1afe666c0c14b85a916df189a013b4
Francisco García Macías <garmacfco@gmail.com>
2022-01-04 19:30:57 +0100

    Login II

--- empresa/urls.py
+++ empresa/urls.py
@@ -5,4 +5,6 @@ from empresa import views
 urlpatterns = [
     path('',views.InicioEmpresaView.as_view(), name="page_inicio"),
     path('registro/',views.registrar_cliente, name="registro_cliente"),
+    path('sign_in/', views.sign_in, name="users_login"),
+    # path('logout/', views.sign_out,name="users_logout"),
 ]
