7290cc8c173ca7a45df0d3aa905e93145345a2d6
Julio150994 <munoz.chjul20@cadiz.salesianos.edu>
2022-01-25 11:38:07 +0100

    Formulario de inscripción a proyecto

--- empresa/view_pry.py
+++ empresa/view_pry.py
@@ -48,3 +48,36 @@ def ver_historial_proyectos(request):
     historialProyectos = Proyecto.objects.all()
     context = { 'proyectos': historialProyectos }
     return render(request,'empresa/historial_proyectos.html',context)
+
+def annadir_inscripcion(request):
+    proyecto = ProyectoModelForm()
+    context = {'proyecto': proyecto}
+
+    if request.POST:
+        proyecto = ProyectoModelForm(request.POST)
+        context = {'proyecto': proyecto}
+        
+        if proyecto.is_valid():
+            last_id_categoria = Categoria.objects.last()
+            last_id_empresario = Empleado.objects.last()
+            titulo = request.POST.get("titulo")
+            descripcion = request.POST.get("descripcion")
+            nivel = request.POST.get("nivel")
+            fechaInicio = request.POST.get("fechaInicio")
+            fechaFin = request.POST.get("fechaFin")
+            informeFinal = request.POST.get("informeFinal")
+            idCategoria  = request.POST.get("idCategoria",last_id_categoria)
+            idEmpleado = request.POST.get("idEmpleado",last_id_empresario)
+
+            if titulo is not None or descripcion is not None or nivel is not None or fechaInicio is not None or fechaFin is not None or informeFinal is not None or idCategoria or idEmpleado:
+                nuevo_proyecto = Proyecto(titulo=titulo, descripcion=descripcion, nivel=nivel,
+                fechaInicio=fechaInicio,fechaFin=fechaFin,informeFinal=informeFinal,
+                idCategoria=idCategoria, idEmpleado=idEmpleado)
+                nuevo_proyecto.save()
+                messages.success(request,'Se ha inscrito al proyecto correctamente.')
+                return redirect('historial_pry')
+        else:
+            messages.warning(request,'Faltan datos por introducir.')
+            return redirect('inscripcion_proyecto')
+
+    return render(request, "empresa/inscripcion_proyecto.html",context)
