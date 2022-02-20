2f4b20d1845fb8791bc5cde390ebd6f17452ee7e
Julio150994 <munoz.chjul20@cadiz.salesianos.edu>
2022-02-20 20:48:15 +0100

    Pequeño cambio en fichero serializers.py

--- empresa/serializers.py
+++ empresa/serializers.py
@@ -16,16 +16,8 @@ class ProyectoSerializer(serializers.ModelSerializer):
         fields = ['titulo','descripcion','nivel','fechaInicio','fechaFin','informeFinal']
 
 class ParticipaSerializer(serializers.ModelSerializer):
-    proyectos = ProyectoSerializer(many=True, read_only=True) #read_only=True#
-    #proyectos = serializers.StringRelatedField(many=True)
+    proyectos = ProyectoSerializer(many=True, read_only=True)
     
     class Meta:
         model = Participa
         fields = ['proyectos','fechaInscripcion','rol']
-        
-    def create(self, validated_data):
-        proyectos_data = validated_data.pop('proyectos')
-        proyecto = Proyecto.objects.create(**validated_data)
-        for proyecto_data in proyectos_data:
-            Proyecto.objects.create(proyecto=proyecto, **proyecto_data)
-        return proyecto
