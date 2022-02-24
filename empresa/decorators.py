1af8fd8c80b1420e52590890f0c82f80b1df21ab
Francisco García Macías <garmacfco@gmail.com>
2022-02-24 17:09:43 +0100

    Decoradores retocados
    Se han retocado los decoradores

--- empresa/decorators.py
+++ empresa/decorators.py
@@ -2,38 +2,33 @@ from django.contrib.auth import REDIRECT_FIELD_NAME
 from django.contrib.auth.decorators import user_passes_test
 from django.http import HttpResponseRedirect
 
-from empresa.models import Usuario
 
-
-def EmpleadoDeco(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='users_login'):
+def EmpleadoDeco(function=None, login_url='users_login'):
    
-    actual_decorator = user_passes_test(
-        lambda u: u.is_active and u.is_staff and not u.is_superuser,
-        login_url=login_url,
-        redirect_field_name=redirect_field_name
-    )
+    actual_decorator = user_passes_test(lambda u: u.is_active 
+        and u.is_staff and not u.is_superuser, login_url=login_url,)
+
     if function:
         return actual_decorator(function)
+
     return actual_decorator
 
-def ClienteDeco(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='users_login'):
+def ClienteDeco(function=None, login_url='users_login'):
    
-    actual_decorator = user_passes_test(
-        lambda u: u.is_active and not u.is_superuser and not u.is_staff,
-        login_url=login_url,
-        redirect_field_name=redirect_field_name
-    )
+    actual_decorator = user_passes_test(lambda u: u.is_active 
+        and not u.is_superuser and not u.is_staff, login_url=login_url,)
+
     if function:
         return actual_decorator(function)
+
     return actual_decorator
 
-def Admin(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='users_login'):
+def Admin(function=None, login_url='users_login'):
        
-    actual_decorator = user_passes_test(
-        lambda u: u.is_active and u.is_superuser,
-        login_url=login_url,
-        redirect_field_name=redirect_field_name
-    )
+    actual_decorator = user_passes_test(lambda u: u.is_active 
+        and u.is_superuser, login_url=login_url,)
+
     if function:
         return actual_decorator(function)
+
     return actual_decorator
